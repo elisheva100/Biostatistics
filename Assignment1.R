0d01ac9c204a8490256d05ff359f8ec0e570b953
elisheva100 <elisheva.h02@gmail.com>
2019-03-03 23:54:51 +0200

    Add files via upload

---
+++ Assignment1.R
@@ -0,0 +1,72 @@
+############################
+#Assignment 1
+#Elisheva Heilbrun 208833012
+############################
+
+library(DAAG)
+setwd("C:/Users/Owner/Desktop/RCourse") #My personal path.
+
+#1.
+mySum = sqrt(67) + sqrt(95) 
+#2.
+isDivided = FALSE # A boolean flag.
+if(!(14576%%7)) #In case 14576 is divisible by 7, the result will be 0.
+{
+  isDivided = TRUE
+}
+#3.
+first = 36*72
+second = 44+1769
+third = first*second
+#4.
+roundedNum = round(.04678, digits = 3)
+#5.
+vec <- c(2.5,98.4,12.9,54.2)
+manualMean = sum(vec)/length(vec)
+autoMean = mean(vec)
+#6.
+myVec = c(1.5,6,8,4.3)
+mySd = sd(myVec)
+#7.
+print(3 > 2)
+print(3 > 2 || 2 > 3)
+print(2 > 3)
+print(3 > 2 && 2 > 3)
+#8.
+print(paste(c("We", "love", "learning", "R"), collapse = " "))
+#9.
+mySeq = 55:64
+print(paste(mySeq[length(mySeq)] -1, mySeq[length(mySeq)]), sep = " ")
+#10.
+filteredA <- A[-c(6, 21)] #Where A is a given vector.
+#11.
+x <- 1:50
+y <- seq(1, 100, by = 2)
+z <- x*y
+m <- seq(5,17, length.out = 10)
+#12
+people <- c("Alex","Lilly","Mark", "Oliver", "Martha", "Lucas", "Caroline" )
+Age <- c(25, 31, 23, 52, 76, 49, 26)
+Height <- c(177, 163, 190, 179, 163, 183, 164)
+Weight <- c(57, 69, 83, 75 ,70, 83, 53)
+Sex <- c("F", "F", "M", "M", "F", "M", "F")
+data1 <- data.frame(Age, Height, Weight, Sex)
+row.names(data1) <- people
+#13.
+Working <- c("Yes", "No", "No", "Yes", "Yes", "No", "Yes")
+data2 <- data.frame(Working)
+row.names(data2) <- people
+#14.
+data3 <- cbind(data1, data2)
+dim(data3)
+sapply(data3, class)
+#15.
+class(state.center)
+state.center.df <- as.data.frame(state.center)
+#16.
+dim(rainforest)
+colnames(rainforest)
+sum(is.na(rainforest$rootsk))
+rainforest <- cbind(rainforest, rainforest$dbh*5)
+colnames(rainforest)[colnames(rainforest) == 'rainforest$dbh * 5'] <- "New column" #Change the new column name.
+write.csv(rainforest, file = "rainforest.csv")
---
+++ Assignment2.R
@@ -0,0 +1,72 @@
+############################
+#Assignment 2
+#Elisheva Heilbrun 208833012
+############################
+
+###############
+#Mtcars dataset
+###############
+attach(mtcars)
+#1
+boxplot(mpg~carb,data=mtcars, main="Car Milage by carburetors ", 
+        xlab="Number of carburetors", ylab="Miles Per Gallon", col = rainbow(6))
+legend("topright", legend = as.character(c(1:4, 6, 8)), fill = rainbow(6) )
+#2
+plot(mtcars[1:5], main = "mtcars scatterplot matrix")
+#3
+mtcars$gear <- factor(mtcars$gear)
+#4.a
+colors.gears <- c("red", "blue", "green")[mtcars$gear] #Define colors acorrding to mtcars$gear values.
+plot(disp~mpg, data = mtcars, main = "Displacement vs car mileage",
+     xlab = "Miles Per Gallon", ylab = "Displacement",
+     col = colors.gears, pch = 18, cex = 1.5)
+legend("topright", title = "Number of gears", legend = levels(mtcars$gear),
+       fill = c("red", "blue", "green"), cex = 0.75)
+abline(lm(disp~mpg, data = mtcars)) #Add regression line.
+legend("bottomleft", legend = paste0("R-squared = ", format(summary(lm(disp~mpg, data = mtcars))$r.squared, digits = 3)), cex = 0.75) #Add R2.
+#4.b
+plot(disp~drat, data = mtcars, main = "Displacement vs rear axle ratio",
+     xlab = "Rear axle ratio" , ylab = "Displacement",
+     col = colors.gears, pch = '*', cex = 1.5)
+legend("topright", title = "Number of gears", legend = levels(mtcars$gear), fill = c("red", "blue", "green"))
+abline(lm(disp~drat, data = mtcars))
+legend("bottomleft", legend = paste0("R-squared = ", format(summary(lm(disp~drat, data = mtcars))$r.squared, digits = 3)), cex = 0.75)
+#5
+"Answer: Not exactly, in the second graph (4.b), I expected a lower value."
+#6
+hist(mtcars$mpg, xlim = c(1,35), col = "red", density = 10, main = "Histograms of mtcars variables",  xlab = "")
+hist(mtcars$qsec, xlim = c(1,35), col = "blue", density = 10, add = TRUE)
+hist(mtcars$carb, xlim = c(1,35), col = "green", density = 10, add = TRUE)
+legend("topright", legend = c("Miles/gallon", "1/4 mile time", "# of carburetors"),
+       fill = c("red", "blue", "green"), cex = 0.75)
+
+##############
+#Trees dataset
+##############
+attach(trees)
+#1.
+mytable <- table(trees$Height, trees$Volume)
+#2
+margin.table(mytable, 1) # Height frequencies (summed over Volume)
+#3
+margin.table(mytable, 2) # Volume frequencies (summed over Height)
+#4
+table(trees$Girth, trees$Height, trees$Volume)
+#5
+a <- c(70, 65, 63, 72, 80, 83, 66, 75, 80, 75, 79, 76, 76, 69, 75, 74, 85, 8, 71, 63, 78, 80, 74,
+    72, 77, 81, 82, 80, 86, 80, 87)
+trees <- cbind(trees, a)
+#6
+mytable_2 <- table(trees$Height, trees$a)
+#7
+View(mytable) #Observe the content.
+View(mytable_2)
+#Thes are 2 different matrices - their columns are different and therefore they have different values.
+#8.a
+print(summary(mytable))
+#8.b
+print(summary(mytable_2))
+#9
+# The chi square test for independence, indicates wether there a statistical dependence between the groups
+# For the Heigth and volume comparison the p-value (obtaind by the Chi-squared test) > 0.05 and therfore, there is no significant relationship between the groups.
+#While for the comparison between the Height and a, p-value < 0.05, and therefore there is a significant relationship between them.
